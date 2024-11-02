3388c78e1188a02734fcbcb45834a33d1835d9d4
Hokage <oyebolatomilade@gmail.com>
2024-11-02 22:10:27 +0000

    Read csv files in a cleaner way

--- leet/Working_with_csv/csv_reader.py
+++ leet/Working_with_csv/csv_reader.py
@@ -1,9 +1,16 @@
 import csv
 import os
 
-with open("test.csv", 'r') as file:
-    csv_reader = csv.reader(file)
-    for row in csv_reader:
-        print(row)
+# with open("test.csv", 'r') as file:
+#     csv_reader = csv.reader(file)
+#     for row in csv_reader:
+#         print(row)
 # print(csv_reader)
 
+import csv
+file = open("test.csv")
+csv_f = csv.reader(file)
+for row in csv_f:
+    Name, Age, City = row
+    print("Name: {}, Age: {}, City: {}".format(Name, Age, City))
+file.close()
